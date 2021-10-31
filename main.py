0e08dc3166e2ef9e4ef1276f76959a9a0e3a1fec
artemmokinn <artemmokinn@gmail.com>
2021-10-31 23:31:45 +0500

    add qr-code.png

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ qr = qrcode.QRCode(
     border=10,
 )
 
-link = ''  # write a link here
+link = 'https://github.com/artemmokinn/qr-code-generator'  # write a link here
 
 qr.add_data(link)
 qr.make(fit=True)
+
 img = qr.make_image(fill="black", back_color="white")
 img.save("qr-code.png")
